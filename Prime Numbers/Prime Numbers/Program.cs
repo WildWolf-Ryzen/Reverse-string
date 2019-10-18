14fe9fb5dfb49bc92bdee8d12ec55b44ba5ed37b
Dino Jovanov <jovanovdino@gmail.com>
2019-10-18 19:23:15 +0200

    Prime Number

---
+++ Prime Numbers/Prime Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to check if its prime or not.");
+            int number, a=0;
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i<=number; i++)
+            {
+                if(number % i == 0)
+                {
+                    a++;
+                }
+
+            }
+            if(a == 2)
+            {
+                Console.WriteLine($"{number} is a prime number!");
+            }
+            else
+                Console.WriteLine($"{number} is not a prime number!");
+
+
+        }
+    }
+}
