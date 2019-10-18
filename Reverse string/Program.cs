abc69e7fd703b710e25872ead26b00851f6ef9e5
Dino Jovanov <jovanovdino@gmail.com>
2019-10-18 18:56:59 +0200

    Add project files.

---
+++ Reverse string/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reverseString = "Jovanov";
+            var charArray = reverseString.ToCharArray();
+
+            Console.WriteLine("Reversed:");
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                Array.Reverse(charArray);
+                Console.WriteLine(charArray[i]);
+            }
+        }
+    }
+}
